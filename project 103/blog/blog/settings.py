927e03cb4a8b7637103f021c421f0ff0e92d6579
Natani <Natani12getnet@gmail.com>
2026-07-03 18:09:58 +0300

    Fix import os order before STATIC_ROOT

--- project 103/blog/blog/settings.py
+++ project 103/blog/blog/settings.py
@@ -120,9 +120,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
+import os
+
 STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-import os
 # The filesystem path where images will be saved
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
