ff99316677ecf96ca80265fc5ff93d5b065afe13
Natani <Natani12getnet@gmail.com>
2026-07-03 18:14:30 +0300

    Add Render domain to ALLOWED_HOSTS, fix import os order

--- project 103/blog/blog/settings.py
+++ project 103/blog/blog/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -25,7 +26,7 @@ SECRET_KEY = 'django-insecure-85+-84_$@&zv%2k=ymd0^d5sbeq&ff=@-0!o5d1+opkw9)f2it
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', 'localhost,127.0.0.1,project-103-2-3.onrender.com').split(',')
 
 
 # Application definition
@@ -119,8 +120,6 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
-
-import os
 
 STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
