1fd9d1c80a7b6e58432a720267313a8ca7e5bab3
Natani <Natani12getnet@gmail.com>
2026-07-03 18:03:36 +0300

    Add STATIC_ROOT for collectstatic

--- project 103/blog/blog/settings.py
+++ project 103/blog/blog/settings.py
@@ -121,6 +121,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 import os
 # The filesystem path where images will be saved
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
